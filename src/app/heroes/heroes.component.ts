b4256223cbd40be7add9bd9dec766c136f202bec
Alberto Zanovello <zanovello2002@gmail.com>
2022-01-02 14:10:38 +0100

    5-Add_Navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,8 +12,6 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-  
   constructor(private heroService: HeroService,private messageService: MessageService) {
     this.heroService.getHeroes()
   }
@@ -22,11 +20,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes()
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-  
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
